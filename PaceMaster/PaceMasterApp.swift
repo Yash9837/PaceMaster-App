de7c50f385a462ae3d41ff645b798699b024a60a
Yash Gupta <guptayas9837@gmail.com>
2025-03-03 17:14:21 +0530

    Initial Commit

---
+++ PaceMaster/PaceMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaceMasterApp.swift
+//  PaceMaster
+//
+//  Created by Yash's Mackbook on 03/03/25.
+//
+
+import SwiftUI
+
+@main
+struct PaceMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
